c40a35a3678b374ffd08c650dcd0de11d52b8e22
YMJ936 <aud780@gmail.com>
2023-09-30 14:41:07 +0900

    TIL

--- src/main/java/me/shinsunyoung/springbootdeveloper/dto/AddArticleRequest.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/dto/AddArticleRequest.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import me.shinsunyoung.springbootdeveloper.domain.Article;
 
 @NoArgsConstructor //기본 생성자 추가
-@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 추가
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 추가한다
 @Getter
 public class AddArticleRequest {
     private String title;
